b7e503847e95c044340934cd11a94fb295365d62
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-01-12 18:16:40 +0100

    Setup Azure GitHub Auth

--- terraform/stacks/bootstrap/init.tf
+++ terraform/stacks/bootstrap/init.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = "1.6.6"
 
   required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.47.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.85.0"
