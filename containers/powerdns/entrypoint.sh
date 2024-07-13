dbf9e800d6f8f43ccf695f51fd003acfaeaecb1b
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-07-13 22:58:41 +0200

    feat(dns): replace bind with powerdns
    Bind & routeros's file api don't like each others very much

---
+++ containers/powerdns/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+DATA_DIR=/var/lib/powerdns
+DATA_FILE="$DATA_DIR/pdns.sqlite3"
+
+# Mikrotik stuff
+if [ ! -f "$DATA_DIR/.type" ]; then
+	touch "$DATA_DIR/.type"
+fi
+
+if [ ! -f $DATA_FILE ]; then
+	echo "Creating database"
+	sqlite3 $DATA_FILE </usr/share/doc/pdns/schema.sqlite3.sql
+	chmod 755 $DATA_FILE
+	chown pdns:pdns $DATA_FILE
+	echo "Done"
+fi
+
+cat <<EOF >/etc/pdns/pdns.conf
+launch=gsqlite3
+gsqlite3-database=$DATA_FILE
+api=yes
+api-key=${API_KEY:-changeme}
+webserver=yes
+webserver-address=${WEBSERVER_ADDRESS:-0.0.0.0}
+webserver-allow-from=${WEBSERVER_ALLOW_FROM:-0.0.0.0/0}
+EOF
+
+exec "$@"
