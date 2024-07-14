cbcee84c3267fe8b261f774fcdf8fdb58b388208
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-07-14 16:57:49 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -15,10 +15,6 @@
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
         inputs.devenv.flakeModule
         inputs.treefmt-nix.flakeModule
       ];
@@ -31,47 +27,40 @@
       perSystem =
         {
           config,
-          self',
           inputs',
           pkgs,
           system,
           ...
         }:
         {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             config.allowUnfree = true;
           };
 
           devenv.shells.default = {
-            packages = with pkgs; [
-              config.treefmt.build.wrapper
-              azure-cli
-              (ansible.overrideAttrs (oldAttrs: {
-                propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ pkgs.python311Packages.librouteros ];
-              }))
-              ansible-lint
-              ansible-language-server
-              age
-              sops
-              terragrunt
-              terraform-ls
-              talosctl
-              timoni
-              cue
-              fluxcd
-              cilium-cli
-              hubble
-              kubernetes-helm
-              earthly
-              inputs'.dagger.packages.dagger
-              opentofu
-              k3s
-            ];
+            packages =
+              [
+                config.treefmt.build.wrapper
+                inputs'.dagger.packages.dagger
+              ]
+              ++ (with pkgs; [
+                azure-cli
+                age
+                sops
+                terragrunt
+                terraform-ls
+                talosctl
+                timoni
+                cue
+                fluxcd
+                cilium-cli
+                hubble
+                kubernetes-helm
+                earthly
+                opentofu
+                pdns
+              ]);
           };
 
           treefmt = {
@@ -82,6 +71,7 @@
             programs.nixfmt.package = pkgs.nixfmt-rfc-style;
             programs.terraform.enable = true;
             programs.cue.enable = true;
+            programs.shfmt.enable = true;
           };
         };
     };
