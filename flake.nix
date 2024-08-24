608d4a56da63a4bcf7c4c57bd7cf569c4f5a3cdf
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-08-24 15:27:30 +0200

    chore(flake): switch from devenv to devshell

--- flake.nix
+++ flake.nix
@@ -5,18 +5,27 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     dagger.url = "github:dagger/nix";
     dagger.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-topology.url = "github:oddlama/nix-topology";
+    nix-topology.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.devenv.flakeModule
+        inputs.devshell.flakeModule
         inputs.treefmt-nix.flakeModule
+        inputs.nix-topology.flakeModule
       ];
       systems = [
         "x86_64-linux"
@@ -38,7 +47,7 @@
             config.allowUnfree = true;
           };
 
-          devenv.shells.default = {
+          devshells.default = {
             packages =
               [
                 config.treefmt.build.wrapper
@@ -65,6 +74,7 @@
                 pdns
                 containerlab
                 gopls
+                terraform-backend-git
               ]);
           };
 
@@ -81,6 +91,16 @@
             programs.gofmt.enable = true;
             programs.yamlfmt.enable = true;
           };
+
+          topology.modules = [
+            {
+              networks.lan = {
+                name = "lan";
+                cidrv4 = "10.0.100.0/24";
+
+              };
+            }
+          ];
         };
     };
 }
