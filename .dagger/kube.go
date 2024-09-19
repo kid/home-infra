3533e0778497a79a535694edbe3593e72c988f17
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-09-19 15:24:16 +0200

    Install flux-local in kube base container

--- .dagger/kube.go
+++ .dagger/kube.go
@@ -41,12 +41,20 @@ func (m *Kube) buildChecks(ctx context.Context) (checks []Check, err error) {
 func (m *Kube) Base() *dagger.Container {
 	kubeConform := dag.Container().From("ghcr.io/yannh/kubeconform:v0.6.7-alpine")
 	return m.HomeInfra.
-		Base("kustomize=5.4.3").
+		Base(
+			"kustomize=5.4.3",
+			"flux=2.3.0",
+			"helm=3.16.1",
+			"py3-pip",
+			"git=2.46.1",
+		).
+		WithExec([]string{"pip3", "install", "flux-local"}).
 		WithFile("/usr/bin/kubeconform", kubeConform.File("/kubeconform")).
 		WithNewFile("/usr/bin/kubeconform-kustomize", string(kubeconformKustomize), dagger.ContainerWithNewFileOpts{
 			Permissions: 0755,
 		}).
-		WithDirectory("/src", m.HomeInfra.Source).
+		WithDirectory("/src/clusters", m.HomeInfra.Source.Directory("clusters")).
+		WithDirectory("/src/.git", m.HomeInfra.GitDir).
 		WithWorkdir("/src")
 }
 
