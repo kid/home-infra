feb1065d2b0a7b9f6a2fe8c8761162bff2de59b3
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-12-30 13:12:40 +0100

    fix: skip validation of sops secrets

--- .dagger/scripts/kubeconform-kustomize.sh
+++ .dagger/scripts/kubeconform-kustomize.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 format="${FORMAT:-pretty}"
 
-kustomize build "$@" | kubeconform \
-  -strict \
-  -verbose \
-  -summary \
-  -output "$format" \
-  -schema-location default \
-  -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
-  -schema-location 'https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/v3/apis__apiextensions.k8s.io__v1_openapi.json'
+kustomizeArgs=(
+	--enable-helm
+	--load-restrictor=LoadRestrictionsNone
+)
+kubeconformArgs=(
+	-strict
+	-verbose
+	-summary
+	-output "$format"
+	-schema-location default
+	-schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json'
+	-schema-location 'https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/v3/apis__apiextensions.k8s.io__v1_openapi.json'
+)
+
+kustomize build "${kustomizeArgs[@]}" "$@" | yq "del(.sops)" | kubeconform "${kubeconformArgs[@]}"
