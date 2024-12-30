b616f77e570f0f03d9cc0a17499b6c8b36603c41
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-12-30 12:56:11 +0100

    fix: update terraform providers

--- terraform/stacks/talos-cluster/versions.tf
+++ terraform/stacks/talos-cluster/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     routeros = {
       source  = "terraform-routeros/routeros"
-      version = "1.66.0"
+      version = "1.72.0"
     }
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.68.0"
+      version = "0.69.0"
     }
     talos = {
       source  = "siderolabs/talos"
