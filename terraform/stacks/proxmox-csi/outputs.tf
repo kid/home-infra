493a705fd5f52317902ade95fe38859bcd224993
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-01-19 10:41:23 +0100

    fix: url for proxmox-csi

--- terraform/stacks/proxmox-csi/outputs.tf
+++ terraform/stacks/proxmox-csi/outputs.tf
@@ -1,5 +1,5 @@
 output "proxmox_url" {
-  value     = data.sops_file.proxmox.data.proxmox_endpoint
+  value     = "${data.sops_file.proxmox.data.proxmox_endpointS}/api2/json"
   sensitive = true
 }
 
