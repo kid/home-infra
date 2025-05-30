3b034c28cd01f6c847426db911988d805db2fa3a
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-05-30 10:00:52 +0200

    fix: more tweaks

--- terraform/modules/ros-management-config/main.tf
+++ terraform/modules/ros-management-config/main.tf
@@ -1,42 +1,43 @@
-# TODO: set router identity
 # TODO: import SSH keys
-# TODO: add dhcp-server on management subnet
-# TODO: add dhcp-client on admin vlan
 # TODO: implement VRF for services
+# TODO: implement ROMON when resource is available
+# TODO: routerboard auto upgrade
 
 locals {
-  oob_mgmt_cidr = "${var.oob_mgmt_cidr_prefix}/${var.oob_mgmt_cidr_bits}"
+  mgmt_cidr = "${var.mgmt_cidr_prefix}/${var.mgmt_cidr_bits}"
 }
 
-resource "routeros_interface_list" "admin" {
-  name = "admin-ifces"
+resource "routeros_system_identity" "self" {
+  name = var.hostname
 }
 
-resource "routeros_interface_list_member" "admin_interface" {
-  list      = routeros_interface_list.admin.name
-  interface = var.oob_mgmt_interface
-}
+# resource "routeros_interface_list" "mgmt" {
+#   name = "list-mgmt"
+# }
+#
+# resource "routeros_interface_list_member" "mgmt_port" {
+#   list      = routeros_interface_list.mgmt.name
+#   interface = var.oob_mgmt_port
+# }
+#
+# resource "routeros_interface_list_member" "mgmt_vlan" {
+#   list      = routeros_interface_list.mgmt.name
+#   interface = routeros_interface_vlan.mgmt.name
+# }
 
-resource "routeros_interface_list_member" "admin_vlan" {
-  list      = routeros_interface_list.admin.name
-  interface = routeros_interface_vlan.admin.name
-}
-
-resource "routeros_interface_vlan" "admin" {
+resource "routeros_interface_vlan" "mgmt" {
+  name      = "vlan-mgmt"
   interface = var.bridge_name
-  name      = "admin-vlan"
   vlan_id   = var.mgmt_vlan_id
 }
 
-resource "routeros_ip_address" "oob" {
-  interface = var.oob_mgmt_interface
-  address   = "${cidrhost(local.oob_mgmt_cidr, 1)}/${var.oob_mgmt_cidr_bits}"
+resource "routeros_ip_address" "mgmt" {
+  interface = routeros_interface_vlan.mgmt.name
+  address   = "${cidrhost(local.mgmt_cidr, var.mgmt_hostnum)}/${var.mgmt_cidr_bits}"
 }
 
-module "oob_dhcp" {
-  source = "../ros-dhcp"
-
-  interface   = var.oob_mgmt_interface
-  cidr_prefix = var.oob_mgmt_cidr_prefix
-  cidr_bits   = var.oob_mgmt_cidr_bits
+# TODO: remove when we have OSPF?
+resource "routeros_ip_route" "gateway" {
+  dst_address = "0.0.0.0/0"
+  gateway     = cidrhost(local.mgmt_cidr, 1)
 }
